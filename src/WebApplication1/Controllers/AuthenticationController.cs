6775ff7ee85dfb7def516661048a9f7b4d2b8b57
N-Rem <nicolasromero.barrios@gmail.com>
2024-09-11 20:54:28 -0300

    Update, add try and catch.

--- src/WebApplication1/Controllers/AuthenticationController.cs
+++ src/WebApplication1/Controllers/AuthenticationController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Interfaces;
 using Application.Models.Requests;
+using Domain.Exceptions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,9 +23,22 @@ namespace API.Controllers
         [HttpPost]
         public ActionResult<string> Authenticate([FromBody] AuthenticationRequest authenticationRequest)
         {
+            try
+            {
             //Llama a un metodo que devuelve un string-Token
             string token = _authenticationService.Authenticate(authenticationRequest);
             return Ok(token);
+            }
+            catch (NotAllowedException ex)
+            {
+                // problemas del lado del cliente
+                return BadRequest(new { Message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                // problema de conectividad, etc.
+                return StatusCode(500, "An unexpected error occurred.");
+            }
         }
     }
 }
